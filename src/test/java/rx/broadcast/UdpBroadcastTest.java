d7acaccad16cf8c040788f7b7dbf6bedb59a9ddc
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-02-17 18:04:27 -0330

    Autoclose resources after each test

--- src/test/java/rx/broadcast/UdpBroadcastTest.java
+++ src/test/java/rx/broadcast/UdpBroadcastTest.java
@@ -1,22 +1,51 @@
 package rx.broadcast;
 
+import org.junit.After;
 import org.junit.Test;
 import rx.Observable;
 import rx.observers.TestSubscriber;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Supplier;
 
 public class UdpBroadcastTest {
+    private final List<Closeable> resources = new LinkedList<>();
+
+    private final Supplier<DatagramSocket> datagramSocketSupplier = () -> {
+        try {
+            final DatagramSocket socket = new DatagramSocket();
+            resources.add(socket);
+            return socket;
+        } catch (final SocketException e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @After
+    public final void tearDown() {
+        resources.forEach(closeable -> {
+            try {
+                closeable.close();
+            } catch (final IOException e) {
+                // ???
+            }
+        });
+    }
+
     @SuppressWarnings({"checkstyle:magicnumber"})
     @Test
-    public final void valuesOfTypeDoesReceiveBroadcastValue() throws SocketException {
+    public final void valuesOfTypeDoesReceiveBroadcastValue() {
         final TestSubscriber<Object> subscriber = new TestSubscriber<>();
-        final DatagramSocket sa = new DatagramSocket();
-        final DatagramSocket sb = new DatagramSocket();
+        final DatagramSocket sa = datagramSocketSupplier.get();
+        final DatagramSocket sb = datagramSocketSupplier.get();
         final Broadcast broadcast1 = new UdpBroadcast(sa, InetAddress.getLoopbackAddress(), sb.getLocalPort());
         final Broadcast broadcast2 = new UdpBroadcast(sb, InetAddress.getLoopbackAddress(), sa.getLocalPort());
 
@@ -28,17 +57,14 @@ public class UdpBroadcastTest {
         subscriber.assertNoErrors();
         subscriber.assertValueCount(1);
         subscriber.assertReceivedOnNext(Collections.singletonList(new TestValue(42)));
-
-        sa.close();
-        sb.close();
     }
 
     @SuppressWarnings({"checkstyle:magicnumber"})
     @Test
-    public final void eachSubscriptionDoesSendTheBroadcast() throws SocketException {
+    public final void eachSubscriptionDoesSendTheBroadcast() {
         final TestSubscriber<Object> subscriber = new TestSubscriber<>();
-        final DatagramSocket sa = new DatagramSocket();
-        final DatagramSocket sb = new DatagramSocket();
+        final DatagramSocket sa = datagramSocketSupplier.get();
+        final DatagramSocket sb = datagramSocketSupplier.get();
         final Broadcast broadcast1 = new UdpBroadcast(sa, InetAddress.getLoopbackAddress(), sb.getLocalPort());
         final Broadcast broadcast2 = new UdpBroadcast(sb, InetAddress.getLoopbackAddress(), sa.getLocalPort());
 
@@ -56,8 +82,5 @@ public class UdpBroadcastTest {
         subscriber.assertValueCount(4);
         subscriber.assertReceivedOnNext(Arrays.asList(
             new TestValue(42), new TestValue(42), new TestValue(42), new TestValue(42)));
-
-        sa.close();
-        sb.close();
     }
 }
