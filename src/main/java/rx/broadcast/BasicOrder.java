07f6349d2775d3923d1cc0e5e13462e8d0b3a2b1
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-03-30 20:36:02 -0230

    Add JavaDoc comments to BasicOrder class

--- src/main/java/rx/broadcast/BasicOrder.java
+++ src/main/java/rx/broadcast/BasicOrder.java
@@ -2,12 +2,23 @@ package rx.broadcast;
 
 import java.util.function.Consumer;
 
+/**
+ * A basic ordering of broadcasted values. {@code BasicOrder} provides no guarantees of message order atop
+ * what is implemented in the broadcast implementation.
+ * @param <T> the type of the message
+ */
 public final class BasicOrder<T> implements BroadcastOrder<T, T> {
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public T prepare(final T value) {
         return value;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void receive(final long sender, final Consumer<T> consumer, final T message) {
         consumer.accept(message);
