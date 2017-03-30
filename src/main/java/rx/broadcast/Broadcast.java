5cc9944239757ad8efa6be3b3a20850a166eccf3
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-03-30 20:24:36 -0230

    Add JavaDoc comments to Broadcast

--- src/main/java/rx/broadcast/Broadcast.java
+++ src/main/java/rx/broadcast/Broadcast.java
@@ -2,8 +2,22 @@ package rx.broadcast;
 
 import rx.Observable;
 
+/**
+ * A {@code Broadcast} represents both a producer and subscriber of messages.
+ */
 public interface Broadcast {
+    /**
+     * Broadcasts the given value when the returned {@code Observable} is subscribed to.
+     * @param value the value to broadcast
+     * @return an Observable stream representing the message sent status
+     */
     Observable<Void> send(Object value);
 
+    /**
+     * Returns a (hot) {@code Observable} stream of broadcasted values.
+     * @param clazz the class type to filter the broadcasted values by
+     * @param <T> the output stream type
+     * @return an Observable stream of broadcasted values
+     */
     <T> Observable<T> valuesOfType(Class<T> clazz);
 }
