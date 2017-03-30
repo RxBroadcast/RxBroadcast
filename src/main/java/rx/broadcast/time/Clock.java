8755402d5e58aa916f5d3b870cb7db6e442491e5
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-03-30 19:42:45 -0230

    Add JavaDoc comments for rx.broadcast.time classes

--- src/main/java/rx/broadcast/time/Clock.java
+++ src/main/java/rx/broadcast/time/Clock.java
@@ -2,6 +2,16 @@ package rx.broadcast.time;
 
 import java.util.function.LongFunction;
 
+/**
+ * A clock used to generate timestamps for a message.
+ */
 public interface Clock {
+    /**
+     * Executes a single tick of the clock, generating a timestamp and calling
+     * the given ticker function with the timestamp and returning the result.
+     * @param ticker a function of a timestamp
+     * @param <T> the type of the result of the ticker
+     * @return the result of the ticker function
+     */
     <T> T tick(LongFunction<T> ticker);
 }
