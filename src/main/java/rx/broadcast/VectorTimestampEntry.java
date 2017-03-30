a0caec6382965202ff26d91310f0934aae471f6c
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-03-30 20:55:16 -0230

    Add JavaDoc comments to VectorTimestamp and related classes

--- src/main/java/rx/broadcast/VectorTimestampEntry.java
+++ src/main/java/rx/broadcast/VectorTimestampEntry.java
@@ -1,12 +1,30 @@
 package rx.broadcast;
 
+/**
+ * An entry in a vector timestamp, containing an ID of a message sender (called
+ * a process or machine in some literature).
+ */
 final class VectorTimestampEntry {
     final long id;
 
     final long timestamp;
 
+    /**
+     * Creates an instance of {@code VectorTimestampEntry}.
+     * @param id an ID
+     * @param timestamp the timestamp value
+     */
     VectorTimestampEntry(final long id, final long timestamp) {
         this.id = id;
         this.timestamp = timestamp;
+    }
+
+    /**
+     * Returns a string identifying this timestamp entry.
+     * @return a string identifying this timestamp entry
+     */
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", id, timestamp);
     }
 }
--- src/main/java/rx/broadcast/VectorTimestamped.java
+++ src/main/java/rx/broadcast/VectorTimestamped.java
@@ -1,5 +1,9 @@
 package rx.broadcast;
 
+/**
+ * Represents a value of type {@code T} that has been timestamped with a {@code VectorTimestamp}.
+ * @param <T> the type of the timestamped value.
+ */
 @SuppressWarnings("WeakerAccess")
 final class VectorTimestamped<T> {
     public VectorTimestamp timestamp;
@@ -11,6 +15,12 @@ final class VectorTimestamped<T> {
 
     }
 
+    /**
+     * Creates an instance of {@code VectorTimestamped} that associates
+     * the given vector {@code timestamp} with the given {@code value}.
+     * @param value the timestamped value
+     * @param timestamp the timestamp
+     */
     public VectorTimestamped(final T value, final VectorTimestamp timestamp) {
         this.timestamp = timestamp;
         this.value = value;
