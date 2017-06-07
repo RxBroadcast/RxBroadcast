1d1aff29e05141f536cd960ed7a6595bb411bac6
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-06-07 20:30:14 -0230

    Annotate DaemonThreadFactory#newThread as NotNull

--- src/main/java/rx/broadcast/DaemonThreadFactory.java
+++ src/main/java/rx/broadcast/DaemonThreadFactory.java
@@ -1,13 +1,16 @@
 package rx.broadcast;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
 class DaemonThreadFactory implements ThreadFactory {
     private final AtomicLong id = new AtomicLong();
 
+    @NotNull
     @Override
-    public final Thread newThread(final Runnable r) {
+    public final Thread newThread(@NotNull final Runnable r) {
         final Thread t = new Thread(r);
         t.setDaemon(true);
         t.setName("RxBroadcast-" + id.incrementAndGet());
