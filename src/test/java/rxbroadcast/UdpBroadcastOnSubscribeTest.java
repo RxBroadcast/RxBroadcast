fa628ad8cfaeb10c2f6225d75300ba25c0061f98
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-10-22 20:29:01 -0230

    Replace socket housekeeping with try-with-resources in tests

--- src/test/java/rxbroadcast/UdpBroadcastOnSubscribeTest.java
+++ src/test/java/rxbroadcast/UdpBroadcastOnSubscribeTest.java
@@ -1,59 +1,33 @@
 package rxbroadcast;
 
-import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import rx.observers.TestSubscriber;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Supplier;
 
 @SuppressWarnings({"checkstyle:MagicNumber"})
 public final class UdpBroadcastOnSubscribeTest {
-    private final List<Closeable> resources = new LinkedList<>();
-
-    private final Supplier<DatagramSocket> datagramSocketSupplier = () -> {
-        try {
-            final DatagramSocket socket = new DatagramSocket();
-            resources.add(socket);
-            return socket;
-        } catch (final SocketException e) {
-            throw new RuntimeException(e);
-        }
-    };
-
     @Rule
     public final Timeout timeout = Timeout.seconds(1);
-
-    @After
-    public final void tearDown() {
-        resources.forEach(closeable -> {
-            try {
-                closeable.close();
-            } catch (final IOException e) {
-                // ???
-            }
-        });
-    }
 
     @Test
     public final void unsubscribeDoesBreakCall() throws SocketException {
         final TestSubscriber<Object> subscriber = new TestSubscriber<>();
-        final UdpBroadcastOnSubscribe<Object> onSubscribe = new UdpBroadcastOnSubscribe<>(
-            datagramSocketSupplier.get(), new ObjectSerializer<>(), new NoOrder<>());
+        try (final DatagramSocket readSocket = new DatagramSocket()) {
+            final UdpBroadcastOnSubscribe<Object> onSubscribe = new UdpBroadcastOnSubscribe<>(
+                readSocket, new ObjectSerializer<>(), new NoOrder<>());
 
-        subscriber.unsubscribe();
-        onSubscribe.call(subscriber);
+            subscriber.unsubscribe();
+            onSubscribe.call(subscriber);
 
-        subscriber.assertNoErrors();
-        subscriber.assertNoValues();
-        subscriber.assertNotCompleted();
+            subscriber.assertNoErrors();
+            subscriber.assertNoValues();
+            subscriber.assertNotCompleted();
+        }
     }
 
     @Test
