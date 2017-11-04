726e7c96af18e5f8c9b8464096ee9a529d370ee0
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-11-04 19:33:32 -0230

    Add destinationPort to PingPongUdpNoOrder tests

--- src/test/java/rxbroadcast/integration/pp/PingPongUdpNoOrderObjectSerializer.java
+++ src/test/java/rxbroadcast/integration/pp/PingPongUdpNoOrderObjectSerializer.java
@@ -10,11 +10,13 @@ import rx.Observable;
 import rx.observers.TestSubscriber;
 
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
+@SuppressWarnings({"checkstyle:MagicNumber", "checkstyle:AvoidInlineConditionals"})
 public final class PingPongUdpNoOrderObjectSerializer {
     private static final int MESSAGE_COUNT = 100;
 
@@ -27,12 +29,20 @@ public final class PingPongUdpNoOrderObjectSerializer {
      */
     @Test
     public final void recv() throws SocketException, UnknownHostException {
-        final int port = Integer.parseInt(System.getProperty("port"));
-        final InetSocketAddress destination = new InetSocketAddress(System.getProperty("destination"), port);
+        final int port = System.getProperty("port") != null
+            ? Integer.parseInt(System.getProperty("port"))
+            : 54321;
+        final int destinationPort = System.getProperty("destinationPort") != null
+            ? Integer.parseInt(System.getProperty("destinationPort"))
+            : 12345;
+        final InetAddress destination = System.getProperty("destination") != null
+            ? InetAddress.getByName(System.getProperty("destination"))
+            : InetAddress.getLoopbackAddress();
+        final InetSocketAddress destinationSocket = new InetSocketAddress(destination, destinationPort);
         final TestSubscriber<Ping> subscriber = new TestSubscriber<>();
         try (final DatagramSocket socket = new DatagramSocket(port)) {
             final Broadcast broadcast = new UdpBroadcast<>(
-                socket, destination, new ObjectSerializer<>(), new NoOrder<>());
+                socket, destinationSocket, new ObjectSerializer<>(), new NoOrder<>());
 
             broadcast.valuesOfType(Ping.class)
                 .doOnNext(System.out::println)
@@ -60,11 +70,19 @@ public final class PingPongUdpNoOrderObjectSerializer {
      * @throws UnknownHostException if no IP address for the host machine could be found.
      */
     public static void main(final String[] args) throws InterruptedException, SocketException, UnknownHostException {
-        final int port = Integer.parseInt(System.getProperty("port"));
-        final InetSocketAddress destination = new InetSocketAddress(System.getProperty("destination"), port);
+        final int port = System.getProperty("port") != null
+            ? Integer.parseInt(System.getProperty("port"))
+            : 54321;
+        final int destinationPort = System.getProperty("destinationPort") != null
+            ? Integer.parseInt(System.getProperty("destinationPort"))
+            : 12345;
+        final InetAddress destination = System.getProperty("destination") != null
+            ? InetAddress.getByName(System.getProperty("destination"))
+            : InetAddress.getLoopbackAddress();
+        final InetSocketAddress destinationSocket = new InetSocketAddress(destination, destinationPort);
         try (final DatagramSocket socket = new DatagramSocket(port)) {
             final Broadcast broadcast = new UdpBroadcast<>(
-                socket, destination, new ObjectSerializer<>(), new NoOrder<>());
+                socket, destinationSocket, new ObjectSerializer<>(), new NoOrder<>());
 
             Observable.range(1, MESSAGE_COUNT)
                 .map(Ping::new)
