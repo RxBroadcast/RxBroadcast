27537bbf5944b3270452ee8e1bfe7a51de329769
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-05-30 14:34:47 -0230

    Fix PMD.CloseResource errors

--- src/test/java/rxbroadcast/WhoAmITest.java
+++ src/test/java/rxbroadcast/WhoAmITest.java
@@ -13,21 +13,19 @@ public final class WhoAmITest {
     @Test
     public final void sendRecvDoesCloseItsSocket() throws IOException {
         final WhoAmI whoAmI = new WhoAmI(12345, false);
-        final DatagramSocket socket = new DatagramSocket();
-
-        whoAmI.sendRecv(socket);
-
-        Assert.assertTrue(socket.isClosed());
+        try (final DatagramSocket socket = new DatagramSocket()) {
+            whoAmI.sendRecv(socket);
+            Assert.assertTrue(socket.isClosed());
+        }
     }
 
     @Test
     public final void ipv4DoesResultInIpv4Address() throws IOException {
         final boolean isIPv6 = false;
         final WhoAmI whoAmI = new WhoAmI(12345, isIPv6);
-        final DatagramSocket socket = new DatagramSocket();
-
-        final DatagramPacket packet = whoAmI.sendRecv(socket);
-
-        Assert.assertTrue(packet.getAddress() instanceof Inet4Address);
+        try (final DatagramSocket socket = new DatagramSocket()) {
+            final DatagramPacket packet = whoAmI.sendRecv(socket);
+            Assert.assertTrue(packet.getAddress() instanceof Inet4Address);
+        }
     }
 }
